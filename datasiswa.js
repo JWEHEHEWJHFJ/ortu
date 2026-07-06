b579ed4d444bf7e94d5bbcd40a0a8fbcc2f71532
JWEHEHEWJHFJ <walikusman5pinrang@gmail.com>
2026-07-06 09:59:37 +0800

    [portal] Update datasiswa.js via Apps Script

--- datasiswa.js
+++ datasiswa.js
@@ -3,7 +3,7 @@
  * File ini di-generate otomatis oleh upload_datasiswa.gs
  * ⚠️  Jangan edit manual — gunakan script upload untuk memperbarui.
  *
- * Terakhir diperbarui: 2026-07-06 09:54:33
+ * Terakhir diperbarui: 2026-07-06 09:59:16
  */
 
 window.SISWA_DATA = {
