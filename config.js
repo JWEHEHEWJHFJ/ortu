2ed4a1e8b35316815352e16ceb29132a26e88e95
JWEHEHEWJHFJ <walikusman5pinrang@gmail.com>
2026-07-17 22:55:27 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-window.GITHUB_CONFIG = {
+window.CONFIG = {
   
   gasUrl: "https://script.google.com/macros/s/AKfycbyCJP2pymmqwQRFI6_-gY6r7hh8AgNWIsqIEanE2Ix6A36LekclA1Map6PtUg90-R3q/exec",   //pass    
 };
